17ff560e77c7ff1b0adf26110dd29682a0a62fd7
Noah Franco <noah@macbooks-mbp.lan>
2020-10-28 13:52:55 -0400

    passed down props to display likes and comments by maping through state

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import Comment from './Comment';
-import './Comments.css';
+import React from "react";
+import Comment from "./Comment";
+import "./Comments.css";
 
-const Comments = props => {
+const Comments = (props) => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
+  console.log("comment props: ", props.comments);
 
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
+      {props.comments.map((res) => (
+        <Comment key={res.id} data={res} />
+      ))}
     </div>
   );
 };
--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -1,29 +1,34 @@
-import React from 'react';
-import Comments from '../Comments/Comments';
-import LikeSection from './LikeSection';
-import PostHeader from './PostHeader';
+import React, { useState } from "react";
+import Comments from "../Comments/Comments";
+import LikeSection from "./LikeSection";
+import PostHeader from "./PostHeader";
 
-const Post = props => {
+const Post = (props) => {
   // 🔥 Make sure the parent of Post is passing the right props!
   const { post, likePost } = props;
+  // console.log("props from post: ", props.data);
+
+  const [likes, setLikes] = useState(props.data.likes);
+
+  const [comments, setComments] = useState(props.data.comments);
 
   return (
-    <div className='post-border'>
+    <div className="post-border">
       <PostHeader
-        username={post.username}
-        thumbnailUrl={post.thumbnailUrl}
+        username={props.data.username}
+        thumbnailUrl={props.data.thumbnailUrl}
       />
-      <div className='post-image-wrapper'>
+      <div className="post-image-wrapper">
         <img
-          alt='post thumbnail'
-          className='post-image'
-          src={post.imageUrl}
+          alt="post thumbnail"
+          className="post-image"
+          src={props.data.imageUrl}
         />
       </div>
-      {/* Is LikeSection getting all the props it needs to work correctly? */}
-      <LikeSection likePost={() => likePost(post.id)} />
+      {/* Is LikeSection getting all the props it needs to work correctly?  */}
+      <LikeSection likes={likes} likePost={() => likePost(props.data.id)} />
       {/* Comments also wants its props! */}
-      <Comments />
+      <Comments comments={comments} />
     </div>
   );
 };
